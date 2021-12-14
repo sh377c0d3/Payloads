4508141c49efb765f8fe570a558d4ec982f25f0f
Manish Sharma <62472469+sh377c0d3@users.noreply.github.com>
2021-12-14 17:20:59 +0530

    Create port_scanner.py

---
+++ Fuzzing Script/port_scanner.py
@@ -0,0 +1,17 @@
+from socket import *
+import time
+startTime = time.time()
+
+if __name__ == '__main__':
+   target = input('Enter the host to be scanned: ')
+   t_IP = gethostbyname(target)
+   print ('Starting scan on host: ', t_IP)
+
+   for i in range(0, 65535):
+      s = socket(AF_INET, SOCK_STREAM)
+
+      conn = s.connect_ex((t_IP, i))
+      if(conn == 0) :
+         print ('Port %d: OPEN' % (i,))
+      s.close()
+print('Time taken:', time.time() - startTime)
