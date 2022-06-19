5a0bea6bdc21088e6ae31068d95a4040fd28a7cd
Manish Sharma <62472469+sh377c0d3@users.noreply.github.com>
2022-06-19 18:23:21 +0530

    Create bin2shell.py

---
+++ Shellcode/Bin2shellcode/bin2shell.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print "usage: %s file.bin\n" % (sys.argv[0],)
+		sys.exit(0)
+
+	shellcode = "\""
+	ctr = 1
+	maxlen = 15
+
+	for b in open(sys.argv[1], "rb").read():
+		shellcode += "\\x" + b.encode("hex")
+		if ctr == maxlen:
+			shellcode += "\" +\n\""
+			ctr = 0
+		ctr += 1
+	shellcode += "\""
+	print shellcode
