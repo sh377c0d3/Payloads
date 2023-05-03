2fc7fe351d349dfd9b1b6e7e998fba1555e7080d
N.Pranav Krishna <52345192+npranav7619@users.noreply.github.com>
2023-05-03 20:30:14 +0530

    Updated
    A simple scripts that installs other programs and outputs the result.

---
+++ Forensics/for3ntic.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#!/bin/bash
+set -ex
+
+OS=$(uname -s | tr A-Z a-z)
+
+case $OS in
+  linux)
+    source /etc/os-release
+    case $ID in
+      debian|ubuntu|mint)
+        apt install exiftool
+        apt install stegsnow
+        apt install stegno
+        ;;
+      arch)
+        pacman -S exiftool stegsnow stegno
+        ;;
+      *)
+        echo -n "unsupported linux distro  manually install exiftool,stegsnow,stegno"
+        ;;
+    esac
+  ;;
+esac
+echo "name of the file : "
+read filename
+exiftool $filename >> Result.txt
+echo "Stegsnow : " >> Result.txt
+stegsnow $filename >> Result.txt
+echo "Stegno : " >> Result.txt
+stegno $filename >> Result.txt
+
+echo "Done...Check Results.txt "
